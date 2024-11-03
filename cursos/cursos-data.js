3f5472b55edc9864d79c694b6b0e27ecfd1a4825
Prof André Ronconi <112502859+AndreRonconi@users.noreply.github.com>
2024-11-03 15:53:22 -0300

    new videos on course

--- cursos/cursos-data.js
+++ cursos/cursos-data.js
@@ -2,53 +2,53 @@ export default {
   curso1: {
     videos: [
       {
-        name: 'Unit 01 - Bem vindo ao mundo Inglês!',
+        name: 'Unit 01 - Bem vindo ao Inglês!',
         description: 'Numbers / Colors / Days of the week',
         cover: '/assets/img/videos/capas/01.jpg',
-        preview: '/assets/img/videos/00.mp4',
+        preview: '/assets/img/videos/01.mp4',
         link: 'https://www.youtube.com/embed/hWAs0F15WSk'
       },
       {
         name: 'Unit 02 - Olá! Como você está?',
         description: 'Greetings / Nationalities / Verb to be',
         cover: '/assets/img/videos/capas/02.jpg',
-        preview: '/assets/img/videos/01.mp4',
+        preview: '/assets/img/videos/02.mp4',
         link: 'https://www.youtube.com/embed/kXCc_J0vAZw'
       },
       {
-        name: 'Unit 02 - More about you!',
-        description: 'What is + It is Professions Vocabulary',
+        name: 'Unit 03 - Hotel Majestic',
+        description: 'Hotel / What is - It is',
         cover: '/assets/img/videos/capas/03.jpg',
-        preview: '/assets/img/videos/02.mp4',
-        link: 'https://www.youtube.com/embed/EWZg42x3m4o'
-      },
-      {
-        name: 'Unit 03 - Where is everyone?',
-        description: 'Useful Vocabulary Verb to be (part 2)',
-        cover: '/assets/img/videos/capas/04.jpg',
         preview: '/assets/img/videos/03.mp4',
-        link: 'https://www.youtube.com/embed/5nHlIzk2Fbk'
+        link: 'https://www.youtube.com/embed/1W0U4afEzro'
       },
       {
-        name: 'Unit 04 - My hometown.',
-        description: 'Articles (A, AN and THE) There is + There are',
-        cover: '/assets/img/videos/capas/05.jpg',
+        name: 'Unit 04 - Onde está você?',
+        description: 'Verbs / Prepositions of place',
+        cover: '/assets/img/videos/capas/04.jpg',
         preview: '/assets/img/videos/04.mp4',
-        link: 'https://www.youtube.com/embed/3Yu5nwBQXus'
+        link: 'https://www.youtube.com/embed/6NDJbt68x80'
       },
       {
-        name: 'Unit 05 - Who is that?',
-        description: 'Family Vocabulary Simple Present',
-        cover: '/assets/img/videos/capas/06.jpg',
+        name: 'Unit 05 - Como é sua cidade natal?',
+        description: 'Articles (A, AN and THE) / There is-are',
+        cover: '/assets/img/videos/capas/05.jpg',
         preview: '/assets/img/videos/05.mp4',
-        link: 'https://www.youtube.com/embed/xnoR07vYT00'
+        link: 'https://www.youtube.com/embed/tOAFqq7HX54'
       },
       {
-        name: 'Unit 06 - Why don\'t we go out?',
-        description: 'Question Words Adverbs os frequency',
-        cover: '/assets/img/videos/capas/07.jpg',
+        name: 'Unit 06 - Minha família é demais!',
+        description: 'Family Vocabulary / Simple Present',
+        cover: '/assets/img/videos/capas/06.jpg',
         preview: '/assets/img/videos/06.mp4',
-        link: 'https://www.youtube.com/embed/jC2trxp7KNI'
+        link: 'https://www.youtube.com/embed/dfIugxTYJ5k'
+      },
+      {
+        name: 'Unit 07 - Você assiste TV?',
+        description: 'WH Question Words / Adverbs os frequency',
+        cover: '/assets/img/videos/capas/07.jpg',
+        preview: '/assets/img/videos/07.mp4',
+        link: 'https://www.youtube.com/embed/QjdSDaQrL4A'
       },
     ]
   }
