f7e150fc4bc1009fc1925f7519094627e6a69e19
Prof André Ronconi <112502859+AndreRonconi@users.noreply.github.com>
2024-11-03 15:25:39 -0300

    new videos on courses

--- cursos/cursos-data.js
+++ cursos/cursos-data.js
@@ -2,11 +2,11 @@ export default {
   curso1: {
     videos: [
       {
-        name: 'Unit 00 - Saia do básico!',
-        description: 'Greetings Nationalities Verb to be (part 1)',
-        cover: '/assets/img/videos/capas/00.jpg',
+        name: 'Unit 01 - Bem vindo ao mundo Inglês!',
+        description: 'Numbers / Colors / Days of the week',
+        cover: '/assets/img/videos/capas/00.png',
         preview: '/assets/img/videos/00.mp4',
-        link: 'https://www.youtube.com/embed/d47ZIuPDcXA'
+        link: 'https://www.youtube.com/embed/hWAs0F15WSk'
       },
       {
         name: 'Unit 01 - Getting to know...',
@@ -49,34 +49,6 @@ export default {
         cover: '/assets/img/videos/capas/06.jpg',
         preview: '/assets/img/videos/06.mp4',
         link: 'https://www.youtube.com/embed/jC2trxp7KNI'
-      },
-      {
-        name: 'Unit 07 - What\'s for dinner?',
-        description: 'Food Vocabulary Quantifiers',
-        cover: '/assets/img/videos/capas/07.jpg',
-        preview: '/assets/img/videos/07.mp4',
-        link: 'https://www.youtube.com/embed/9mTZWq2vaZw'
-      },
-      {
-        name: 'Unit 08 - Let\'s go shopping!',
-        description: 'Shopping Vocabulary Demonstrative Pronouns',
-        cover: '/assets/img/videos/capas/08.jpg',
-        preview: '/assets/img/videos/08.mp4',
-        link: 'https://www.youtube.com/embed/-BD7veRCWUY'
-      },
-      {
-        name: 'Unit 09 - Plans for the weekend.',
-        description: 'Weather vocabulary Present Continuous',
-        cover: '/assets/img/videos/capas/09.jpg',
-        preview: '/assets/img/videos/09.mp4',
-        link: 'https://www.youtube.com/embed/CLyw-sl0Co0'
-      },
-      {
-        name: 'Unit 10 - Get well soon.',
-        description: 'Sports and Symptoms vocabulary PS x PC',
-        cover: '/assets/img/videos/capas/10.jpg',
-        preview: '/assets/img/videos/10.mp4',
-        link: 'https://www.youtube.com/embed/Gq8XmuzbQHA'
       },
     ]
   }
