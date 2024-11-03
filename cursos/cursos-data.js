567f60a742b75a3c792b849f216ee5cc77caa67c
Prof André Ronconi <112502859+AndreRonconi@users.noreply.github.com>
2024-11-03 16:14:30 -0300

    new videos covers

--- cursos/cursos-data.js
+++ cursos/cursos-data.js
@@ -12,42 +12,42 @@ export default {
         name: 'Unit 02 - Olá! Como você está?',
         description: 'Greetings / Nationalities / Verb to be',
         cover: '/assets/img/videos/capas/02.jpg',
-        preview: '/assets/img/videos/02.mp4',
+        preview: '/assets/img/videos/capas/02b.jpg',
         link: 'https://www.youtube.com/embed/kXCc_J0vAZw'
       },
       {
         name: 'Unit 03 - Hotel Majestic',
         description: 'Hotel / What is - It is',
         cover: '/assets/img/videos/capas/03.jpg',
-        preview: '/assets/img/videos/03.mp4',
+        preview: '/assets/img/videos/capas/03b.jpg',
         link: 'https://www.youtube.com/embed/1W0U4afEzro'
       },
       {
         name: 'Unit 04 - Onde está você?',
         description: 'Verbs / Prepositions of place',
         cover: '/assets/img/videos/capas/04.jpg',
-        preview: '/assets/img/videos/04.mp4',
+        preview: '/assets/img/videos/capas/04b.jpg',
         link: 'https://www.youtube.com/embed/6NDJbt68x80'
       },
       {
         name: 'Unit 05 - Como é sua cidade natal?',
         description: 'Articles (A, AN and THE) / There is-are',
         cover: '/assets/img/videos/capas/05.jpg',
-        preview: '/assets/img/videos/05.mp4',
+        preview: '/assets/img/videos/capas/05b.jpg',
         link: 'https://www.youtube.com/embed/tOAFqq7HX54'
       },
       {
         name: 'Unit 06 - Minha família é demais!',
         description: 'Family Vocabulary / Simple Present',
         cover: '/assets/img/videos/capas/06.jpg',
-        preview: '/assets/img/videos/06.mp4',
+        preview: '/assets/img/videos/capas/06b.jpg',
         link: 'https://www.youtube.com/embed/dfIugxTYJ5k'
       },
       {
         name: 'Unit 07 - Você assiste TV?',
         description: 'WH Question Words / Adverbs os frequency',
         cover: '/assets/img/videos/capas/07.jpg',
-        preview: '/assets/img/videos/07.mp4',
+        preview: '/assets/img/videos/capas/07b.jpg',
         link: 'https://www.youtube.com/embed/QjdSDaQrL4A'
       },
     ]
