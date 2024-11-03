54cdb0a47c47e3e7fb7b18514179b8c77f0316a2
Prof André Ronconi <112502859+AndreRonconi@users.noreply.github.com>
2024-11-03 15:34:52 -0300

    new videos on courses

--- cursos/cursos-data.js
+++ cursos/cursos-data.js
@@ -4,49 +4,49 @@ export default {
       {
         name: 'Unit 01 - Bem vindo ao mundo Inglês!',
         description: 'Numbers / Colors / Days of the week',
-        cover: '/assets/img/videos/capas/00.png',
+        cover: '/assets/img/videos/capas/01.jpg',
         preview: '/assets/img/videos/00.mp4',
         link: 'https://www.youtube.com/embed/hWAs0F15WSk'
       },
       {
-        name: 'Unit 01 - Getting to know...',
-        description: 'Numbers / Colors / Days of the week',
-        cover: '/assets/img/videos/capas/01.jpg',
+        name: 'Unit 02 - Olá! Como você está?',
+        description: 'Greetings / Nationalities / Verb to be',
+        cover: '/assets/img/videos/capas/02.jpg',
         preview: '/assets/img/videos/01.mp4',
-        link: 'https://www.youtube.com/embed/2sZxL20Bfq4'
+        link: 'https://www.youtube.com/embed/kXCc_J0vAZw'
       },
       {
         name: 'Unit 02 - More about you!',
         description: 'What is + It is Professions Vocabulary',
-        cover: '/assets/img/videos/capas/02.jpg',
+        cover: '/assets/img/videos/capas/03.jpg',
         preview: '/assets/img/videos/02.mp4',
         link: 'https://www.youtube.com/embed/EWZg42x3m4o'
       },
       {
         name: 'Unit 03 - Where is everyone?',
         description: 'Useful Vocabulary Verb to be (part 2)',
-        cover: '/assets/img/videos/capas/03.jpg',
+        cover: '/assets/img/videos/capas/04.jpg',
         preview: '/assets/img/videos/03.mp4',
         link: 'https://www.youtube.com/embed/5nHlIzk2Fbk'
       },
       {
         name: 'Unit 04 - My hometown.',
         description: 'Articles (A, AN and THE) There is + There are',
-        cover: '/assets/img/videos/capas/04.jpg',
+        cover: '/assets/img/videos/capas/05.jpg',
         preview: '/assets/img/videos/04.mp4',
         link: 'https://www.youtube.com/embed/3Yu5nwBQXus'
       },
       {
         name: 'Unit 05 - Who is that?',
         description: 'Family Vocabulary Simple Present',
-        cover: '/assets/img/videos/capas/05.jpg',
+        cover: '/assets/img/videos/capas/06.jpg',
         preview: '/assets/img/videos/05.mp4',
         link: 'https://www.youtube.com/embed/xnoR07vYT00'
       },
       {
         name: 'Unit 06 - Why don\'t we go out?',
         description: 'Question Words Adverbs os frequency',
-        cover: '/assets/img/videos/capas/06.jpg',
+        cover: '/assets/img/videos/capas/07.jpg',
         preview: '/assets/img/videos/06.mp4',
         link: 'https://www.youtube.com/embed/jC2trxp7KNI'
       },
