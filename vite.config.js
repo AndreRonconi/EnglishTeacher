9fd273d2821d998d93109c1dfad31ba2cf9bb548
Prof André Ronconi <112502859+AndreRonconi@users.noreply.github.com>
2025-05-21 12:22:40 -0300

    viteconfig adjusted

--- vite.config.js
+++ vite.config.js
@@ -47,6 +47,7 @@ export default defineConfig({
         clicaqui: resolve(__dirname, "clique_aqui/index.html"),
         smart: resolve(__dirname, "smart.html"),
         test: resolve(__dirname, "index2.html"),
+        planos: resolve(__dirname, "planos.html"),
       },
     },
   },
